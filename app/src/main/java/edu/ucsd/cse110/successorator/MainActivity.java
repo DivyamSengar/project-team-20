f91b01cdcf23410312f5fff3ce3d44e92517f9fd
Kevin Zheng <kezheng@ucsd.edu>
2024-02-09 02:51:52 -0800

    Third smaller change
    Added the rule for where code is gotten from.

--- app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/MainActivity.java
@@ -17,9 +17,18 @@ public class MainActivity extends AppCompatActivity {
 
         var view = ActivityMainBinding.inflate(getLayoutInflater(), null, false);
 
-        //Code got from https://www.geeksforgeeks.org/how-to-get-current-time-and-date-in-android/
-        SimpleDateFormat sdf = new SimpleDateFormat("EEEE'\n'MM/dd");
-        String currentDate = sdf.format(new Date());
+        /*Code got from:
+        https://www.geeksforgeeks.org/how-to-get-current-time-and-date-in-android/
+        How to Get Current Time and Date in Android?
+        Captured at 2/09/2024
+        Used for copying code to capture the date, and changed the format to fit our format
+        Copied code start-
+        SimpleDateFormat sdf = new SimpleDateFormat("'Date\n'dd-MM-yyyy '\n\nand\n\nTime\n'HH:mm:ss z");
+        String currentDateAndTime = sdf.format(new Date());
+        -end
+        */
+        SimpleDateFormat date = new SimpleDateFormat("EEEE'\n'MM/dd");
+        String currentDate = date.format(new Date());
         view.editTextDate.setText(currentDate);
         //To test the empty goal text
         view.emptyGoals.setText(R.string.emptyGoalsText);
