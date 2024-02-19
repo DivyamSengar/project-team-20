58dd05bf64d965a0c7d5379aa6f6abfe6ff95a31
WHOOP555 <dsengar@ucsd.edu>
2024-02-18 17:40:25 -0800

    time database changes

--- app/src/main/java/edu/ucsd/cse110/successorator/timedata/db/RoomTimeKeeper.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/timedata/db/RoomTimeKeeper.java
@@ -1,9 +1,10 @@
 package edu.ucsd.cse110.successorator.timedata.db;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Transformations;
 
 import java.time.LocalDateTime;
-
+import androidx.lifecycle.LiveData;
 import edu.ucsd.cse110.successorator.data.db.GoalDao;
 import edu.ucsd.cse110.successorator.data.db.GoalEntity;
 import edu.ucsd.cse110.successorator.lib.domain.TimeKeeper;
@@ -19,6 +20,12 @@ public class RoomTimeKeeper implements TimeKeeper {
         var entityLiveData = timeDao.getTimeAsLiveData();
         var TimeLiveData = Transformations.map(entityLiveData, TimeEntity::toLocalDateTime);
         return new LiveDataSubjectAdapter<>(TimeLiveData);
+    }
+
+    public int[] getFields(){
+        var entity = timeDao.getTimeAsLiveData();
+        var liveDataTemp = entity.getValue();
+        return liveDataTemp.Fields();
     }
     public void setDateTime(LocalDateTime dateTime){
         timeDao.insertTime(TimeEntity.fromLocalDateTime(dateTime));
--- app/src/main/java/edu/ucsd/cse110/successorator/timedata/db/TimeEntity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/timedata/db/TimeEntity.java
@@ -17,33 +17,60 @@ public class TimeEntity {
         @ColumnInfo(name = "id")
         public int id;
 
-        @ColumnInfo(name = "localDateTime")
-        public String localDateTime;
+//        @ColumnInfo(name = "localDateTime")
+//        public String localDateTime;
 
-        public TimeEntity(@NonNull String localDateTime, int id) {
-            this.localDateTime = localDateTime;
+        @ColumnInfo(name = "hour")
+        public int hour;
+
+    @ColumnInfo(name = "day")
+    public int day;
+
+    @ColumnInfo(name = "minutes")
+    public int minutes;
+
+    @ColumnInfo(name = "month")
+    public int month;
+
+    @ColumnInfo(name = "year")
+    public int year;
+
+        public TimeEntity(int id, int minutes, int hour, int day, int month, int year) {
+//            this.localDateTime = localDateTime;
             this.id = id;
+            this.day = day;
+            this. hour = hour;
+            this.minutes = minutes;
+            this.year = year;
+            this.month = month;
         }
 
         public static TimeEntity fromLocalDateTime(@NonNull LocalDateTime localDateTime){
-            return new TimeEntity(LocalDateTimeToString(localDateTime), 0);
+            return new TimeEntity(0, localDateTime.getMinute(),
+                    localDateTime.getHour(), localDateTime.getDayOfMonth(),
+                    localDateTime.getMonthValue(),localDateTime.getYear());
         }
 
         public @NonNull LocalDateTime toLocalDateTime (){
-            return StringToLocalDateTime(localDateTime);
+            return LocalDateTime.of(year, month, day, hour, minutes);
     }
-    public static String LocalDateTimeToString(LocalDateTime localDateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String formattedDateTime = localDateTime.format(formatter);
-        return formattedDateTime;
+
+    public int[] Fields(){
+            int[] arr = {year, month, day, hour, minutes};
+            return arr;
+    }
+//    public static String LocalDateTimeToString(LocalDateTime localDateTime) {
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        String formattedDateTime = localDateTime.format(formatter);
+//        return formattedDateTime;
+////
+//    }
 //
-    }
-
-    public static LocalDateTime StringToLocalDateTime(String str){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime parsedDateTime = LocalDateTime.parse(str, formatter);
-        return parsedDateTime;
-
-    }
+//    public static LocalDateTime StringToLocalDateTime(String str){
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        LocalDateTime parsedDateTime = LocalDateTime.parse(str, formatter);
+//        return parsedDateTime;
+//
+//    }
 
 }
